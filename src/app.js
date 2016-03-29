babe918022c6033d70f0adeecfb4a4e7cbe17dc3
Giridhar reddy <Giridhar reddy>
2016-03-29 01:06:18 -0500

    total project

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 'use strict';
 var express= require('express'),
     posts=require('./mock/posts.json');
+	
+var postlist=Object.keys(posts).map(function(v){
+	return posts[v];
+});
+	
+
 var app=express();
 app.use('/static',express.static(__dirname+'/public'));
 app.set('views',__dirname+'/templates');
@@ -8,14 +14,16 @@ app.set('view engine','jade');
 console.log(__dirname);
 app.get('/',function(request,response){
 	
+	var path=request.path;
+	response.locals.path=path;
 	response.render('index');
 });
 
  app.get('/blog/:title?',function(request,response){
 	var title=request.params.title;
 	if(title===undefined){
-		response.status(503);
-		response.send("page is under contruction");
+	
+		response.render('blog',{posts : postlist});
 	}else{
 		var post=posts[title] || {};
 		response.render('post',{post : post});
